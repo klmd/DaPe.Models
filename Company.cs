067cbe6188075ac841e6cc6fb055d716d5c07fcb
dape <danielpetrik@outlook.cz>
2023-07-24 15:04:14 +0200

    V20

---
+++ Company.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DaPe.Models;
+
+public class Company
+{
+    public int Id { get; set; }
+    [Required]
+    public string CompanyName { get; set; }
+
+    public string? StreetAddress { get; set; }
+    public string? City { get; set; }
+    public string? PostalCode { get; set; }
+    public string? State { get; set; }
+    public string? Phone { get; set; }
+}
